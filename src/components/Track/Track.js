d1f004c4e2487a4fcf5463a89b14ac58fc845322
toniverger <toniverger@msn.com>
2018-02-09 20:56:46 -0300

    Initial commit

---
+++ src/components/Track/Track.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    }
+
+  renderAction() {
+    if (this.props.onAdd) {
+      return <a className='Track-action' onClick={this.addTrack}>+</a>;
+    } else {
+      return <a className='Track-action' onClick={this.removeTrack}>-</a>;
+    }
+  }
+  addTrack() {
+    this.props.onAdd(this.props.track);
+  }
+  removeTrack() {
+    this.props.onRemove(this.props.track);
+  }
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.track.props.album}</p>
+        </div>
+        {this.renderAction()}
+      </div>
+      );
+  }
+}
+
+
+
+export default Track;
